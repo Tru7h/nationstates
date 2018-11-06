a86202906a3befdaa402b44d4371ad46cd6c4cfa
tru7h <31784707+Tru7h@users.noreply.github.com>
2018-11-05 22:14:19 -0600

    switch from print to logging, fixing and refactoring

--- results_forecast.py
+++ results_forecast.py
@@ -1,6 +1,8 @@
 ''' attempt to forecast best option from probable effects '''
-import pathlib
 import decimal
+import logging
+import pathlib
+import random
 import sys
 import re
 
@@ -8,31 +10,35 @@ import requests
 import lxml.html
 import pandas
 
-float_grp = r'([-+]?\d+\.?\d*)'
-effect_pattern = re.compile(r'{num} to {num} (.+) \(mean {num}\)'.format(num=float_grp))
-simple_pattern = re.compile(r'{num} (.+)'.format(num=float_grp))
+ptrn_grps = dict(num=r'([-+]?\d+(?:\.\d+)?)', census=r'([^\.\d]+)')
+effect_pattern = re.compile('^{num} to {num} {census} \(mean {num}\)$'.format(**ptrn_grps))
+simple_pattern = re.compile('^{num} {census}$'.format(**ptrn_grps))
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
-def get_options(nation=None, issue=None, print_bias='.4', result_thresh=None):
+def get_options(nation=None, issue=None, minimum=None):
     if nation is None:
         nation = input('nation: ')
     if issue is None:
         issue = input('issue: ')
+        if minimum is None:
+            minimum = input('minimum: ') or None
 
-    scales_df, options = build_dataframes(nation, issue, result_thresh)
+    scales_df, options = build_dataframes(nation, issue, minimum)
 
     scales_df.dropna(thresh=2, inplace=True)
     scales_df['sort'] = pandas.Series(abs(scales_df.bias) + (scales_df.bias > 0) / 200, scales_df.index)
     scales_df = scales_df.sort_values(by='sort', ascending=False).drop(['sort'], 1).fillna(0)
     with pandas.option_context('display.max_colwidth', -1):
-        print('\n' + scales_df[abs(scales_df.bias) > decimal.Decimal(print_bias)].to_string())
-        print('\n' + options.to_string(index=False))
-    print('\nhttps://nsindex.net/wiki/NationStates_Issue_No._{issue}\n'.format(issue=issue))
+        logger.info(scales_df[abs(scales_df.bias) > 0].head(18).to_string())
+        logger.info(options.to_string(index=False))
+    logger.info('https://nsindex.net/wiki/NationStates_Issue_No._{issue}\n'.format(issue=issue))
 
-def build_dataframes(nation, issue, result_thresh):
+def build_dataframes(nation, issue, minimum):
     scales_file = pathlib.Path(nation + '_category_scale.csv')
     assert scales_file.is_file(), str(scales_file)
     scales_df = pandas.read_csv(scales_file, names=('census', 'bias'), index_col='census')
-    category_scales = scales_df.to_dict()['bias']
+    category_scales = scales_df.to_dict('dict')['bias']
 
     policies_file = pathlib.Path(nation + '_policy_exclusions.csv')
     if policies_file.is_file():
@@ -42,29 +48,37 @@ def build_dataframes(nation, issue, result_thresh):
         exclusions = ()
 
     url = 'http://www.mwq.dds.nl/ns/results/{issue}.html'
-    page = requests.get(url.format(issue=issue), headers = {'content-type': 'text/html'})
+    page = requests.get(url.format(issue=issue), headers = {'content-type': 'nation/achievement_hunters'})
 
     doc = lxml.html.fromstring(page.content)
-    tr_elements = doc.xpath('//tr')
-    options = pandas.DataFrame(columns=['option', 'net_result'])
-    for result, effects, _ in tr_elements[1:]:
+    title, = doc.xpath('//title')
+    logger.info(title.text_content())
+    cols = 'option,datums,net_result,headline'.split(',')
+    options = pandas.DataFrame(columns=cols)
+    for result, effects, observations in doc.xpath('//tr')[1:]:
+        datums = observations.text_content().strip().splitlines() or '0'
+        datums = int(min(cnt for cnt in datums if '-' not in cnt))
+        if datums < 1:
+            continue
         choice, headline = result.text_content()[1:].split(' ', 1)
         deltas, unparsed_strs = weigh_option(effects)
         if any(excluded in unparsed_strs for excluded in exclusions):
             continue
-        weight = sum(category_scales[category] * normalized_effect for category, normalized_effect in deltas)
-        if result_thresh is not None and weight < float(result_thresh):
+        weight = sum(category_scales[category] * deltas[category] for category in deltas)
+        if minimum is not None and weight < float(minimum):
             continue
-        scales_df[choice] = scales_df.index.map(dict(deltas))
+        scales_df[choice] = [deltas.get(category) for category in scales_df.index]
         extras = split_unparsed_strings(unparsed_strs)
+        cols.extend(key for key in extras if key not in cols)
         headlines = headline.replace('@@NAME@@', nation.title()).split('\n')
         for headline in headlines:
-            option_summary = dict(option=choice, net_result=weight, headline=headline, **extras)
+            option_summary = dict(option=choice, datums=datums, net_result=weight, headline=headline, **extras)
             options = options.append(option_summary, ignore_index=True)
-    return scales_df, options
+            choice = ''
+    return scales_df, options[cols]
 
 def weigh_option(effects):
-    results = []
+    results = {}
     unparsed_strs = []
     for effect in effects:
         effect_str = effect.text_content()
@@ -72,16 +86,14 @@ def weigh_option(effects):
         simple = simple_pattern.search(effect_str)
         if regular:
             low, high, category, mean = (tryfloat(vl) for vl in regular.groups())
-            numer = (high + mean + low)/3
+            numer = high + mean + low
             denom = (high if high > 0 else 0) - (low if low < 0 else 0)
-            normalized_effect =  numer / denom
+            results[category] = numer / denom / 3
         elif simple:
             mean, category = simple.groups()
-            normalized_effect = (float(mean) > 0) - (float(mean) < 0)
+            results[category] = (float(mean) > 0) - (float(mean) < 0)
         else:
             unparsed_strs.append(effect_str)
-            continue
-        results.append((category, normalized_effect))
     return results, unparsed_strs
 
 def tryfloat(string):
@@ -100,5 +112,14 @@ def split_unparsed_strings(unparsed_strs):
         extras[policy] = behavior
     return extras
 
-if __name__ == '__main__':
+def main():
+    c_handler = logging.StreamHandler()
+    c_handler.setLevel(logging.INFO)
+    c_format = logging.Formatter('\n%(message)s')
+    c_handler.setFormatter(c_format)
+    logger.addHandler(c_handler)
+    logger.setLevel(logging.INFO)
     get_options(*sys.argv[1:])
+
+if __name__ == '__main__':
+    main()
