5c4b0fc363c13657ec70fcd07a55fe84530f1480
tru7h <31784707+Tru7h@users.noreply.github.com>
2018-11-27 06:52:43 -0600

    revised option filtering and refactored variable names

--- results_forecast.py
+++ results_forecast.py
@@ -23,25 +23,32 @@ def get_options(nation=None, issue=None, minimum=None):
         issue = input('issue: ')
 
     url = 'http://www.mwq.dds.nl/ns/results/{issue}.html'
-    page = requests.get(url.format(issue=issue), headers = {'content-type': 'nation/achievement_hunters'})
+    page = requests.get(url.format(issue=issue), headers = {'content-type': 'text/html'})
 
     doc = lxml.html.fromstring(page.content)
+    excluded = set()
 
     while True:
-        scales_df, options = build_dataframes(nation, doc, minimum)
+        scales_df, options = build_dataframes(nation, doc, excluded)
 
         scales_df.dropna(thresh=2, inplace=True)
         scales_df['sort'] = pandas.Series(abs(scales_df.bias) + (scales_df.bias > 0) / 200, scales_df.index)
         scales_df = scales_df.sort_values(by='sort', ascending=False).drop(['sort'], 1).fillna(0)
         with pandas.option_context('display.max_colwidth', -1):
-            logger.info(scales_df[abs(scales_df.bias) > 0].head(18).to_string())
+            logger.info(scales_df[abs(scales_df.bias) > 0].to_string())
             logger.info(options.to_string(index=False))
-        minimum = input('\n' + 'minimum: ') or None
-        if not minimum:
+        option = input('\n' + 'select option to drop, restore, or reset all with 0: ')
+        if not option:
             break
+        elif option == '0':
+            excluded = set()
+        elif option in excluded:
+            excluded.remove(option)
+        else:
+            excluded.add(option)
     logger.info('https://nsindex.net/wiki/NationStates_Issue_No._{issue}\n'.format(issue=issue))
 
-def build_dataframes(nation, doc, minimum):
+def build_dataframes(nation, doc, excluded_options):
     scales_file = pathlib.Path(nation + '_category_scale.csv')
     assert scales_file.is_file(), str(scales_file)
     scales_df = pandas.read_csv(scales_file, names=('census', 'bias'), index_col='census')
@@ -50,9 +57,9 @@ def build_dataframes(nation, doc, minimum):
     policies_file = pathlib.Path(nation + '_policy_exclusions.csv')
     if policies_file.is_file():
         df = pandas.read_csv(policies_file, names=('policy', 'change')).dropna()
-        exclusions = tuple('{change} policy: {policy}'.format(**row) for row in df.to_dict('records'))
+        excluded_policy_reforms = tuple('{change} policy: {policy}'.format(**row) for row in df.to_dict('records'))
     else:
-        exclusions = ()
+        excluded_policy_reforms = ()
 
     title, = doc.xpath('//title')
     logger.info(title.text_content())
@@ -61,23 +68,21 @@ def build_dataframes(nation, doc, minimum):
     for result, effects, observations in doc.xpath('//tr')[1:]:
         datums = observations.text_content().strip().splitlines() or '0'
         datums = int(min(cnt for cnt in datums if '-' not in cnt))
-        if datums < 1:
+        option, headline = result.text_content()[1:].split(' ', 1)
+        if datums < 1 or option.replace('.', '') in excluded_options:
             continue
-        choice, headline = result.text_content()[1:].split(' ', 1)
         deltas, unparsed_strs = weigh_option(effects)
         weight = sum(category_scales[category] * deltas[category] for category in deltas)
-        if minimum is not None and weight < float(minimum):
-            continue
-        scales_df[choice] = [deltas.get(category) for category in scales_df.index]
-        if any(excluded in unparsed_strs for excluded in exclusions):
-            choice = ''
+        scales_df[option] = [deltas.get(category) for category in scales_df.index]
+        if any(reform in unparsed_strs for reform in excluded_policy_reforms):
+            option = ''
         extras = split_unparsed_strings(unparsed_strs)
         cols.extend(key for key in extras if key not in cols)
         headlines = headline.replace('@@NAME@@', nation.title()).split('\n')
         for headline in headlines:
-            option_summary = dict(option=choice, datums=datums, net_result=weight, headline=headline, **extras)
+            option_summary = dict(option=option, datums=datums, net_result=weight, headline=headline, **extras)
             options = options.append(option_summary, ignore_index=True)
-            choice = ''
+            option = ''
     return scales_df, options[cols]
 
 def weigh_option(effects):
@@ -90,7 +95,7 @@ def weigh_option(effects):
         if regular:
             low, high, category, mean = (tryfloat(vl) for vl in regular.groups())
             numer = high + mean + low
-            denom = (high if high > 0 else 0) - (low if low < 0 else 0)
+            denom = max(high, 0) - min(low, 0)
             results[category] = numer / denom / 3
         elif simple:
             mean, category = simple.groups()
