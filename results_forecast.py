9650df354526b626940e68ac0f48e9d0266b95c2
tru7h <31784707+Tru7h@users.noreply.github.com>
2018-12-20 20:05:06 -0600

    improved code structuring

--- results_forecast.py
+++ results_forecast.py
@@ -30,28 +30,8 @@ def get_options(nation=None, issue=None):
 
     while True:
         scales_df, options = build_dataframes(nation, doc, excluded)
-
-        scales_df.dropna(thresh=2, inplace=True)
-        if not options.empty:
-            scales_df['magnitude'] = pandas.Series(abs(scales_df.bias), scales_df.index)
-            scales_df['direction'] = pandas.Series(scales_df.bias > 0, scales_df.index)
-            sort_cols = ['magnitude', 'direction']
-            viable_options = options[options.option!='']
-            viable_options = options[:1] if viable_options.empty else viable_options
-            for option in viable_options.sort_values(by='net_result', ascending=False).option:
-                scales_df['abs_' + option] = pandas.Series(abs(scales_df[option]), scales_df.index)
-                sort_cols.append('abs_' + option)
-            scales_df.sort_values(by=sort_cols, ascending=False, inplace=True)
-            scales_df = scales_df.drop(sort_cols, 1).fillna(0)
-        if cumsum:
-            bias_col, *option_cols = scales_df.columns
-            for option in option_cols:
-                scales_df[option] = (scales_df[bias_col] * scales_df[option]).cumsum()
-        with pandas.option_context('display.max_colwidth', -1):
-            scales_df = scales_df[scales_df.bias != 0] if census_filter else scales_df
-            logger.info(scales_df.to_string())
-            logger.info(options.to_string(index=False))
-            logger.info('https://nsindex.net/wiki/NationStates_Issue_No._{issue}\n'.format(issue=issue))
+        summarize_results(scales_df, options, census_filter, cumsum)
+        logger.info('https://nsindex.net/wiki/NationStates_Issue_No._{issue}\n'.format(issue=issue))
         option = input(
             '"f" > toggle zero bias\n'
             '"c" > toggle cumulative summation\n'
@@ -72,7 +52,29 @@ def get_options(nation=None, issue=None):
         else:
             excluded.add(option)
 
-def build_dataframes(nation, doc, excluded_options):
+def summarize_results(scales_df, options, census_filter, cumsum):
+    scales_df.dropna(thresh=2, inplace=True)
+    if not options.empty:
+        scales_df['magnitude'] = pandas.Series(abs(scales_df.bias), scales_df.index)
+        scales_df['direction'] = pandas.Series(scales_df.bias > 0, scales_df.index)
+        sort_cols = ['magnitude', 'direction']
+        viable_options = options[options.option!='']
+        viable_options = options[:1] if viable_options.empty else viable_options
+        for option in viable_options.sort_values(by='net_result', ascending=False).option:
+            scales_df['abs_' + option] = pandas.Series(abs(scales_df[option]), scales_df.index)
+            sort_cols.append('abs_' + option)
+        scales_df.sort_values(by=sort_cols, ascending=False, inplace=True)
+        scales_df = scales_df.drop(sort_cols, 1).fillna(0)
+    if cumsum:
+        bias_col, *option_cols = scales_df.columns
+        for option in option_cols:
+            scales_df[option] = (scales_df[bias_col] * scales_df[option]).cumsum()
+    with pandas.option_context('display.max_colwidth', -1):
+        scales_df = scales_df[scales_df.bias != 0] if census_filter else scales_df
+        logger.info(scales_df.to_string())
+        logger.info(options.to_string(index=False))
+
+def build_dataframes(nation, doc, excluded):
     scales_file = pathlib.Path(nation + '_category_scale.csv')
     assert scales_file.is_file(), str(scales_file)
     scales_df = pandas.read_csv(scales_file, names=('census', 'bias'), index_col='census')
@@ -92,21 +94,21 @@ def build_dataframes(nation, doc, excluded_options):
     for result, effects, observations in doc.xpath('//tr')[1:]:
         datums = observations.text_content().strip().splitlines() or '0'
         datums = int(min(cnt for cnt in datums if '-' not in cnt))
-        option, headline = result.text_content()[1:].split(' ', 1)
-        if datums < 1 or any(excluded in option for excluded in excluded_options):
+        option_text, headline = result.text_content()[1:].split(' ', 1)
+        if datums < 1 or any(digit in option_text for digit in excluded):
             continue
         deltas, unparsed_strs = weigh_option(effects)
         weight = sum(category_scales[category] * deltas[category] for category in deltas)
-        scales_df[option] = [deltas.get(category) for category in scales_df.index]
+        scales_df[option_text] = [deltas.get(category) for category in scales_df.index]
         if any(reform in unparsed_strs for reform in excluded_policy_reforms):
             option = ''
         extras = split_unparsed_strings(unparsed_strs)
         cols.extend(key for key in extras if key not in cols)
         headlines = headline.replace('@@NAME@@', nation.title()).split('\n')
         for headline in headlines:
-            option_summary = dict(option=option, datums=datums, net_result=weight, headline=headline, **extras)
+            option_summary = dict(option=option_text, datums=datums, net_result=weight, headline=headline, **extras)
             options = options.append(option_summary, ignore_index=True)
-            option = ''
+            option_text = ''
     return scales_df, options[cols]
 
 def weigh_option(effects):
